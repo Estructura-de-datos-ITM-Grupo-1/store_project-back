034f7e9a1af4b47088f63cc28a817fc7a0b2c9dc
Chika002 <posstmalonne@gmail.com>
2025-05-24 22:09:58 -0500

    add inventory data

--- store/app/data/inventario_data.py
+++ store/app/data/inventario_data.py
@@ -1,17 +1,18 @@
 import json
 from pathlib import Path
+from typing import List, Dict
 
+DATA_FILE = Path(__file__).parent.parent / "data" / "productos.json"
 
-archivo_inventario = Path("inventario.json")
-
-def cargar_productos():
+def cargar_productos() -> List[Dict]:
     try:
-        with open(archivo_inventario, "r") as file:
+        with open(DATA_FILE, "r") as file:
             return json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
         return []
 
-def guardar_productos(productos):
-    with open(archivo_inventario, "w") as file:
+def guardar_productos(productos: List[Dict]):
+    DATA_FILE.parent.mkdir(exist_ok=True)
+    with open(DATA_FILE, "w") as file:
         json.dump(productos, file, indent=4)
     
