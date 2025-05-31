7a9330834da6af8d4075b4f3fa77dbe259f48590
Chika002 <posstmalonne@gmail.com>
2025-05-30 23:41:01 -0500

    delete inventory models

--- store/app/models/inventario_models.py
+++
@@ -1,8 +0,0 @@
-class Producto:
-    def __init__(self, id, nombre, stock, categoria, precio, activo = True):
-        self.id = id
-        self.nombre = nombre
-        self.stock = stock
-        self.categoria = categoria
-        self.precio = precio
-        self.activo = activo
