764a84510a06bb3b841ab72ddee86f73e3ea015a
aconeop <aconeo79@gmail.com>
2025-06-02 16:16:39 -0500

    cargue path clientes

--- store/app/core/paths.py
+++ store/app/core/paths.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 SERVICIOS_JSON = os.path.abspath(os.path.join(DATA_DIR, "servicios.json"))
-#CLIENTES_JSON = os.path.abspath(os.path.join(DATA_DIR, "clientes.json"))
+CLIENTES_JSON = os.path.abspath(os.path.join(DATA_DIR, "clientes.json"))
 USUARIOS_JSON = os.path.abspath(os.path.join(DATA_DIR, "usuarios.json"))
 AUDITORIA_CLIENTES_JSON = os.path.abspath(os.path.join(DATA_DIR, "auditoria_clientes.json"))
 CUADRE_CAJA_JSON = os.path.abspath(os.path.join(DATA_DIR, "cuadre_caja.json"))
